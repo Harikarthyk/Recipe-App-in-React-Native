1fc1ffd2cd42790c4459ff581f796529913b02ab
Hari_karthyk <Harikarthyk@users.noreply.github.com>
2021-01-01 12:45:32 +0530

    v-1

--- Screens/WishList.js
+++ Screens/WishList.js
@@ -1,6 +1,14 @@
 import React, {useEffect, useState} from 'react';
-import {Dimensions, ScrollView, StyleSheet, Text, View} from 'react-native';
+import {
+  Dimensions,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import {useIsFocused} from '@react-navigation/native';
+import MIcon from 'react-native-vector-icons/MaterialIcons';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const {height} = Dimensions.get('window');
@@ -9,6 +17,11 @@ const WishList = () => {
   const [recipe, setRecipe] = useState([]);
   const isFocused = useIsFocused();
 
+  const minsToHours = (min) => {
+    let hr = Math.round(Number(min / 60));
+    min = Math.round(Number(min % 60));
+    return `${hr} hr ${min} min`;
+  };
   useEffect(() => {
     const getAllItem = async () => {
       try {
@@ -36,7 +49,71 @@ const WishList = () => {
           </Text>
         </View>
       ) : (
-        <ScrollView></ScrollView>
+        <ScrollView style={{marginTop: 10}}>
+          {recipe.map((item, index) => {
+            return (
+              <View
+                key={index}
+                style={{
+                  flex: 1,
+                  marginVertical: 10,
+                  padding: 10,
+                  flexDirection: 'row',
+                  backgroundColor: '#fff',
+                  borderRadius: 9,
+                  elevation: 9,
+                }}>
+                {/* <View style={}> */}
+                <Image source={{uri: item.image}} style={[styles.image]} />
+                {/* </View> */}
+                <View style={[{flex: 0.6}]}>
+                  <Text
+                    style={{
+                      fontSize: 21.2,
+                      color: 'black',
+                      fontWeight: 'bold',
+                      marginRight: 5,
+                    }}>
+                    {item.label}
+                  </Text>
+                  <View style={{marginVertical: 10}}>
+                    <Text
+                      style={{
+                        fontSize: 15,
+                        color: 'black',
+                        fontWeight: 'bold',
+                      }}>
+                      Average Cooking Time
+                    </Text>
+                    <View
+                      style={{
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        marginVertical: 6,
+                      }}>
+                      <MIcon
+                        name="alarm"
+                        size={18}
+                        color="grey"
+                        style={{fontWeight: 'bold', marginRight: 6}}
+                      />
+                      <Text
+                        style={{
+                          fontSize: 13,
+                          fontWeight: 'bold',
+                          color: 'grey',
+                        }}>
+                        {item.totalTime == 0
+                          ? '60 min'
+                          : minsToHours(item.totalTime)}
+                      </Text>
+                    </View>
+                  </View>
+                </View>
+              </View>
+            );
+          })}
+        </ScrollView>
       )}
     </ScrollView>
   );
@@ -46,6 +123,7 @@ const styles = StyleSheet.create({
   container: {
     padding: 20,
     flex: 1,
+    backgroundColor: '#F2F2F2',
   },
   title: {
     fontWeight: 'bold',
@@ -57,6 +135,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textAlign: 'center',
   },
+  image: {flex: 0.4, height: height / 4.2, width: '100%', marginRight: 17},
 });
 
 export default WishList;
