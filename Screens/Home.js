24bef6df07ebad767e1edce0f3876f2ad0655674
Hari_karthyk <Harikarthyk@users.noreply.github.com>
2021-01-04 15:20:32 +0530

    v-1

--- Screens/Home.js
+++ Screens/Home.js
@@ -4,7 +4,7 @@ import 'react-native-gesture-handler';
 import {
   StyleSheet,
   Text,
-  KeyboardAvoidingView,
+  ScrollView,
   View,
   TouchableOpacity,
   Dimensions,
@@ -17,7 +17,6 @@ import {getRecipeByQuery} from '../client';
 import SearchModel from '../Components/SearchModel';
 import Recipe from '../Components/Recipe';
 import FilterModel from '../Components/FilterModel';
-
 const {width} = Dimensions.get('window');
 const {height} = Dimensions.get('window');
 
@@ -47,101 +46,101 @@ const Home = () => {
   }, []);
 
   return (
-    <KeyboardAvoidingView
-      // behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-      style={styles.contianer}>
-      <SearchModel
-        query={query}
-        setQuery={setQuery}
-        setLoading={setLoading}
-        setRecipes={setRecipes}
-      />
-      <View style={[styles.contentContainer]}>
-        <Text style={[styles.title]}>Recipiee</Text>
-        {loading ? (
-          <View style={{height: height / 1.7, justifyContent: 'center'}}>
-            <Text
-              style={{textAlign: 'center', fontWeight: 'bold', fontSize: 22}}>
-              Loading...
-            </Text>
+    <ScrollView>
+      <View style={styles.contianer}>
+        <SearchModel
+          query={query}
+          setQuery={setQuery}
+          setLoading={setLoading}
+          setRecipes={setRecipes}
+        />
+        <View style={[styles.contentContainer]}>
+          <Text style={[styles.title]}>Recipiee</Text>
+          {loading ? (
+            <View style={{height: height / 1.7, justifyContent: 'center'}}>
+              <Text
+                style={{textAlign: 'center', fontWeight: 'bold', fontSize: 22}}>
+                Loading...
+              </Text>
+              <Text
+                style={{
+                  marginTop: 10,
+                  textAlign: 'center',
+                  fontWeight: 'bold',
+                  fontSize: 19,
+                }}>
+                Note : Internet is required
+              </Text>
+              <Text
+                style={{
+                  marginTop: 8,
+                  textAlign: 'center',
+                  fontWeight: 'bold',
+                  fontSize: 19,
+                }}>
+                Wait for an minute
+              </Text>
+            </View>
+          ) : (
+            <>
+              {recipes.length === 0 ? (
+                <View style={{height: height / 1.7, justifyContent: 'center'}}>
+                  <Text
+                    style={{
+                      textAlign: 'center',
+                      fontWeight: 'bold',
+                      fontSize: 22,
+                    }}>
+                    No result Found : (
+                  </Text>
+                </View>
+              ) : (
+                <Recipe data={recipes} />
+              )}
+            </>
+          )}
+        </View>
+        {!showFilter ? (
+          <TouchableOpacity
+            onPress={() => {
+              setShowFilter(true);
+            }}
+            style={styles.filter}>
+            <Icon name="layers" color="#fff" size={20} />
             <Text
               style={{
-                marginTop: 10,
-                textAlign: 'center',
+                color: 'white',
                 fontWeight: 'bold',
-                fontSize: 19,
+                fontSize: 17,
+                marginLeft: 10,
               }}>
-              Note : Internet is required
+              Filter
             </Text>
-            <Text
-              style={{
-                marginTop: 8,
-                textAlign: 'center',
-                fontWeight: 'bold',
-                fontSize: 19,
-              }}>
-              Wait for an minute
-            </Text>
+          </TouchableOpacity>
+        ) : (
+          <Text></Text>
+        )}
+        {showFilter ? (
+          <View style={styles.modelFilter}>
+            <FilterModel
+              setRecipes={setRecipes}
+              query={query.length === 0 ? 'Trending' : query}
+              setShowFilter={setShowFilter}
+              setLoading={setLoading}
+              calories={calories}
+              setCalories={setCalories}
+              to={to}
+              setTo={setTo}
+              health={health}
+              setHealth={setHealth}
+              setQuery={setQuery}
+            />
           </View>
         ) : (
-          <>
-            {recipes.length === 0 ? (
-              <View style={{height: height / 1.7, justifyContent: 'center'}}>
-                <Text
-                  style={{
-                    textAlign: 'center',
-                    fontWeight: 'bold',
-                    fontSize: 22,
-                  }}>
-                  No result Found : (
-                </Text>
-              </View>
-            ) : (
-              <Recipe data={recipes} />
-            )}
-          </>
+          <Text></Text>
         )}
       </View>
-      {!showFilter ? (
-        <TouchableOpacity
-          onPress={() => {
-            setShowFilter(true);
-          }}
-          style={styles.filter}>
-          <Icon name="layers" color="#fff" size={20} />
-          <Text
-            style={{
-              color: 'white',
-              fontWeight: 'bold',
-              fontSize: 17,
-              marginLeft: 10,
-            }}>
-            Filter
-          </Text>
-        </TouchableOpacity>
-      ) : (
-        <Text></Text>
-      )}
-      {showFilter ? (
-        <View style={styles.modelFilter}>
-          <FilterModel
-            setRecipes={setRecipes}
-            query={query.length === 0 ? 'Trending' : query}
-            setShowFilter={setShowFilter}
-            setLoading={setLoading}
-            calories={calories}
-            setCalories={setCalories}
-            to={to}
-            setTo={setTo}
-            health={health}
-            setHealth={setHealth}
-            setQuery={setQuery}
-          />
-        </View>
-      ) : (
-        <Text></Text>
-      )}
-    </KeyboardAvoidingView>
+    </ScrollView>
   );
 };
 
@@ -149,8 +148,10 @@ const styles = StyleSheet.create({
   contianer: {
     padding: 20,
     flex: 1,
-    position: 'relative',
+    height: height - 52,
     justifyContent: 'space-evenly',
+    // backgroundColor: 'red',
+    // alignItems: 'flex-start',
   },
   contentContainer: {
     // paddingBottom: 10,
@@ -169,8 +170,9 @@ const styles = StyleSheet.create({
     marginLeft: 12,
     paddingVertical: 10,
     paddingHorizontal: 15,
-    alignItems: 'center',
+    // alignItems: 'center',
     zIndex: 1,
+    justifyContent: 'center',
     elevation: 2,
     borderRadius: 10,
   },
@@ -179,7 +181,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     bottom: 0,
     width: width,
-    height: height / 1.5,
+    height: height / 1.7,
     borderTopRightRadius: 50,
     borderTopLeftRadius: 50,
     elevation: 10,
